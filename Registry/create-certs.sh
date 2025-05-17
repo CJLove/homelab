683a0a3439b954cfa8796bf4334b5942b78fcf4e
Chris Love <christopherlove68@gmail.com>
2025-05-17 14:54:45 -0700

    Update create-certs.sh and switch Docker UI

--- Registry/create-certs.sh
+++ Registry/create-certs.sh
@@ -92,7 +92,7 @@ if [ ! -f "${PARAM_CA_KEY}" ] || [ ! -f "${PARAM_CA_CRT}" ]; then
   openssl req \
     -new \
     -newkey rsa:2048 \
-    -days 999 \
+    -days 3650 \
     -nodes \
     -x509 \
     -subj "/C=US/ST=CA/L=San Diego/O=Chris Love CA" \
